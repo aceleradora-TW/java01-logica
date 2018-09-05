09dcca59ff1e3f32300a4ce60bbac221d81f5d9a
othman853 <anuar.yasser@gmail.com>
2018-09-05 18:28:45 -0300

    Ola mundo imundo

---
+++ src/main/java/com/thoughtworks/aceleradora/agil/logica01/App.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.aceleradora.agil.logica01;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("******************************");
+        System.out.println("QUERO CAFEEEEEEEEEE");
+        System.out.println("******************************");
+        System.out.println();
+    }
+}
